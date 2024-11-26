3e1451468fda796ab2a7c1e584c827a715582c78
20233tn193 <20233tn193@utez.edu.mx>
2024-11-25 23:02:08 -0600

    primer commit

---
+++ Integradora Casilleros/API_casilleros/src/test/java/com/company/books/backend/api_casilleros/ApiCasillerosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.books.backend.api_casilleros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCasillerosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
