900785362c3890fd1db6342b2798924396ee483f
20233tn193 <20233tn193@utez.edu.mx>
2024-11-25 23:02:08 -0600

    primer commit

---
+++ Integradora Casilleros/API_casilleros/src/main/java/com/company/books/backend/api_casilleros/ApiCasillerosApplication.java
@@ -0,0 +1,13 @@
+package com.company.books.backend.api_casilleros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCasillerosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCasillerosApplication.class, args);
+    }
+
+}
